3112e20a06074afe67e7d744b5c731866cd8bc41
akshay643 <73953645+akshay643@users.noreply.github.com>
2023-06-08 16:46:24 +0530

    like button

--- src/app/blog/newblog/page.js
+++ src/app/blog/newblog/page.js
@@ -7,80 +7,81 @@ import { useRouter } from "next/navigation";
 import ReactQuill from "react-quill";
 import { BaseURL } from "@utils/axiosRoute";
 // import "react-quill/dist/quill.snow.css";
-const NewBlog = () => {
-  const { data: session } = useSession();
-  const [value, setValue] = useState("");
+if (typeof window !== undefined) {
+  const NewBlog = () => {
+    const { data: session } = useSession();
+    const [value, setValue] = useState("");
 
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => {
-    if (!session?.user) {
-      router.push("/");
-    }
-    import("react-quill/dist/quill.snow.css");
-  }, []);
-  const [blogData, setBlogData] = useState({
-    title: "",
-    subtitle: "",
-    description: "",
-    creator: session?.user?.id,
-  });
-
-  const handleBlogSubmit = async (e) => {
-    e.preventDefault();
-    const res = await axios.post(`${BaseURL}/api/blogs`, {
-      ...blogData,
-      description: value,
+    useEffect(() => {
+      if (!session?.user) {
+        router.push("/");
+      }
+      import("react-quill/dist/quill.snow.css");
+    }, []);
+    const [blogData, setBlogData] = useState({
+      title: "",
+      subtitle: "",
+      description: "",
+      creator: session?.user?.id,
     });
-    if (res?.data === "Created") {
-      setBlogData({
-        title: "",
-        subtitle: "",
-        description: "",
+
+    const handleBlogSubmit = async (e) => {
+      e.preventDefault();
+      const res = await axios.post(`${BaseURL}/api/blogs`, {
+        ...blogData,
+        description: value,
       });
-      router.push("/profile");
-      alert(
-        "Congratulations! Your Masterpiece is Born: A Journey into the Realm of Creation"
-      );
-    }
-  };
+      if (res?.data === "Created") {
+        setBlogData({
+          title: "",
+          subtitle: "",
+          description: "",
+        });
+        router.push("/profile");
+        alert(
+          "Congratulations! Your Masterpiece is Born: A Journey into the Realm of Creation"
+        );
+      }
+    };
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setBlogData((prevValue) => ({
-      ...prevValue,
-      [name]: value,
-    }));
-  };
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setBlogData((prevValue) => ({
+        ...prevValue,
+        [name]: value,
+      }));
+    };
 
-  return (
-    <section>
-      <div className="text-center head_text">
-        Craft Your Narrative: Unleash the Power of Words
-      </div>
-      <div className="d-flex flex-column align-items-center bg-transparent">
-        <form className="form w-75  border-0" onSubmit={handleBlogSubmit}>
-          <label className="form-control bg-transparent border-0">
-            Title:
-            <input
-              value={blogData?.title}
-              onChange={handleInputChange}
-              name="title"
-              required
-              className="form-control bg-transparent"
-            />
-          </label>
-          <label className="form-control bg-transparent border-0">
-            Subtitle:
-            <input
-              required
-              value={blogData?.subtitle}
-              onChange={handleInputChange}
-              name="subtitle"
-              className="form-control bg-transparent"
-            />
-          </label>
-          {/* <label className="form-control bg-transparent border-0">
+    return (
+      <section>
+        <div className="text-center head_text">
+          Craft Your Narrative: Unleash the Power of Words
+        </div>
+        <div className="d-flex flex-column align-items-center bg-transparent">
+          <form className="form w-75  border-0" onSubmit={handleBlogSubmit}>
+            <label className="form-control bg-transparent border-0">
+              Title:
+              <input
+                value={blogData?.title}
+                onChange={handleInputChange}
+                name="title"
+                required
+                className="form-control bg-transparent"
+              />
+            </label>
+            <label className="form-control bg-transparent border-0">
+              Subtitle:
+              <input
+                required
+                value={blogData?.subtitle}
+                onChange={handleInputChange}
+                name="subtitle"
+                className="form-control bg-transparent"
+              />
+            </label>
+            {/* <label className="form-control bg-transparent border-0">
             Your Thoughts:
             <textarea
               required
@@ -93,21 +94,21 @@ const NewBlog = () => {
               className="form-control bg-transparent"
             />
           </label> */}
-          {/* <ReactQuill
-            style={{ margin: "1rem" }}
-            theme="snow"
-            value={value}
-            onChange={(e) => setValue(e)}
-          /> */}
-          <div className="text-center mt-4">
-            <button type="submit" className="btn btn-outline-dark">
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </section>
-  );
-};
-
+            <ReactQuill
+              style={{ margin: "1rem" }}
+              theme="snow"
+              value={value}
+              onChange={(e) => setValue(e)}
+            />
+            <div className="text-center mt-4">
+              <button type="submit" className="btn btn-outline-dark">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  };
+}
 export default NewBlog;
