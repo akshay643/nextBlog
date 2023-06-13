4803cab9ef8a3fe02b99f0889902d47c88ef7d0a
akshay643 <73953645+akshay643@users.noreply.github.com>
2023-06-13 13:41:23 +0530

    newBlog page chnages

--- src/app/blog/newblog/page.js
+++ src/app/blog/newblog/page.js
@@ -7,14 +7,71 @@ import { useRouter } from "next/navigation";
 import { BaseURL } from "@utils/axiosRoute";
 import dynamic from "next/dist/shared/lib/dynamic";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+const Quill = dynamic(() => import("react-quill"), { ssr: false });
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "react-quill/dist/quill.snow.css";
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
 
+const colors = [
+  "#000000",
+  "#e60000",
+  "#ff9900",
+  "#ffff00",
+  "#008a00",
+  "#0066cc",
+  "#9933ff",
+  "#ffffff",
+  "#facccc",
+  "#ffebcc",
+  "#ffffcc",
+  "#cce8cc",
+  "#cce0f5",
+  "#ebd6ff",
+  "#bbbbbb",
+  "#f06666",
+  "#ffc266",
+  "#ffff66",
+  "#66b966",
+  "#66a3e0",
+  "#c285ff",
+  "#888888",
+  "#a10000",
+  "#b26b00",
+  "#b2b200",
+  "#006100",
+  "#0047b2",
+  "#6b24b2",
+];
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"],
+  ["blockquote", "code-block"],
+  [{ header: 1 }, { header: 2 }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ direction: "rtl" }],
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ color: colors }, { background: [] }],
+  [{ font: [] }],
+  [{ align: [] }],
+  ["clean"],
+];
 // import "react-quill/dist/quill.snow.css";
 const NewBlog = () => {
   const { data: session } = useSession();
   const [value, setValue] = useState("");
 
   const router = useRouter();
+  const ColorPicker = () => {
+    const ColorAttributor = Quill.import("attributors/class/color");
+    ColorAttributor.whitelist = colors;
+    Quill.register(ColorAttributor, true);
 
+    return null;
+  };
   useEffect(() => {
     if (!session?.user) {
       router.push("/");
@@ -97,6 +154,9 @@ const NewBlog = () => {
             />
           </label> */}
           <ReactQuill
+            modules={{
+              toolbar: toolbarOptions,
+            }}
             style={{ margin: "1rem" }}
             theme="snow"
             value={value}
